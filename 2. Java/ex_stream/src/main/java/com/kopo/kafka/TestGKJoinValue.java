94d6a4e717aa4f9307924498d075d118063b3b2a
Wayne Hwang <wonyong@wonyong.net>
2023-02-21 23:22:12 +0900

    Create TestGKJoinValue.java

---
+++ 2. Java/ex_stream/src/main/java/com/kopo/kafka/TestGKJoinValue.java
@@ -0,0 +1,40 @@
+package com.kopo.kafka;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.GlobalKTable;
+import java.util.Properties;
+
+
+public class TestGKJoin{
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "globalKTalbe-Test");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "master:9092,slave1:9092,slave2:9092");
+        props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> mystream = builder.stream("kopo-topic");
+        GlobalKTable<String, String> chatTable = builder.globalTable("chat-p1topic");
+
+        mystream.join(chatTable,
+                     (mystreamKey, mystreamValue) -> {System.out.println(mystreamKey);System.out.println(mystreamValue); return mystreamValue;},
+                     (mystreamValue, chatTableValue) -> {System.out.println(mystreamValue);System.out.println(chatTableValue);return chatTableValue + " at " + mystreamValue;})
+                     .to("javatopic");
+
+        final Topology topology = builder.build();
+        System.out.println(topology.describe());
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+
+        streams.start();
+        // streams.close();
+    }
+}
