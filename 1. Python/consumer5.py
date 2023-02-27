7bd637ad40afd6a4f4c434e4f6a1034215bd2747
Wayne Hwang <wonyong@wonyong.net>
2023-02-28 08:38:11 +0900

    Update consumer5.py

--- 1. Python/consumer5.py
+++ 1. Python/consumer5.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('news-topic',group_id='kopoconsumer',bootstrap_servers='localhost:9092',enable_auto_commit=True,auto_offset_reset='latest',fetch_min_bytes=1, heartbeat_interval_ms=3000, session_timeout_ms=10000) 
+consumer = KafkaConsumer('news-topic',group_id='kopoconsumer',bootstrap_servers='master:9092,slave1:9092,slave2:9092',enable_auto_commit=True,auto_offset_reset='latest',fetch_min_bytes=1, heartbeat_interval_ms=3000, session_timeout_ms=10000) 
 
 print(".")
 for message in consumer:
