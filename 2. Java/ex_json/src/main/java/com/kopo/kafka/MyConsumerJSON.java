3923e04927c471f02a29ebb5945adc00ab3cab64
Wayne Hwang <wonyong@wonyong.net>
2023-03-09 16:28:23 +0900

    comment out unnecessaries

--- 2. Java/ex_json/src/main/java/com/kopo/kafka/MyConsumerJSON.java
+++ 2. Java/ex_json/src/main/java/com/kopo/kafka/MyConsumerJSON.java
@@ -11,25 +11,19 @@ import java.io.*;
 public class MyConsumerJSON {
 
     public static void main(String[] args) {
-        JsonDeserializer<User> deserializer = new JsonDeserializer<>(User.class);
-        deserializer.setRemoveTypeHeaders(false);
-        deserializer.addTrustedPackages("*");
-        deserializer.setUseTypeMapperForKey(true);
-        Properties configs = new Properties();
         // 환경 변수 설정
+	Properties configs = new Properties();
         configs.put("bootstrap.servers", "master:9092,slave1:9092,slave2:9092");     // kafka server host 및 port
         configs.put("session.timeout.ms", "10000");             // session 설정
         configs.put("group.id", "telegroup");                // group 설정
         configs.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");    // key deserializer
         configs.put("value.deserializer", "org.springframework.kafka.support.serializer.JsonDeserializer");  // value deserializer
-        // configs.put("value.deserializer",deserializer);
-configs.put(JsonDeserializer.TRUSTED_PACKAGES, "com.kopo.kafka");
+        configs.put(JsonDeserializer.TRUSTED_PACKAGES, "com.kopo.kafka");
         configs.put("enable.auto.commit","true");
         configs.put("auto.commit.interval.ms","1000");
         KafkaConsumer<String, Object> consumer = new KafkaConsumer<String, Object>(configs);    // consumer 생성
         consumer.subscribe(Arrays.asList("kopo-topic"));      // topic 설정
 
-        BufferedWriter bw = null;
         while (true) {  // 계속 loop를 돌면서 producer의 message를 띄운다.
             ConsumerRecords<String, Object> records = consumer.poll(500);
             for (ConsumerRecord<String, Object> record : records) {
