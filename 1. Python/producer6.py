76deabb04205d457623542d16e9d69ead17825b9
Wayne Hwang <wonyong@wonyong.net>
2026-04-10 10:42:37 +0900

    Update producer6.py
    add user-agent in header to prevent 403

--- 1. Python/producer6.py
+++ 1. Python/producer6.py
@@ -1,12 +1,14 @@
-import json
+import requests
 from sseclient import SSEClient as EventSource
 
-url = 'https://stream.wikimedia.org/v2/stream/recentchange'
-for event in EventSource(url):
-    if event.event == 'message':
-        try:
-            change = json.loads(event.data)
-        except ValueError:
-            pass
-        else:
-            print('{user} edited {title}'.format(**change))
+url = "https://stream.wikimedia.org/v2/stream/recentchange"
+
+headers = {
+    "User-Agent": "kopo-kafka-practice/1.0 (test@example.com)"
+}
+
+session = requests.Session()
+session.headers.update(headers)
+
+for event in EventSource(url, session=session):
+    print(event.data)
